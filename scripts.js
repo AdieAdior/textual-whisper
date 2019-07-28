332f6e2e4f2422f56b9adcc77975728fe8c755b7
Adie <Kiyiko@gmail.com>
2019-07-28 11:53:38 -0500

    Fix line coalescing and user selection feature
    Fix user selection feature
    
    remove outdated code as per the 6.0.0 migration guide
    
    https://help.codeux.com/textual/release-notes/Style-Developers%3A-Migrating-to-6.0.0.kb

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ Whisper = {
 	},
 
 	getSenderNick: function(sender) {
-		if (sender) { return sender.getAttribute('nickname'); }
+		if (sender) { return sender.getAttribute('data-nickname'); }
 	},
 
 	getPreviousLine: function(line) {
@@ -36,7 +36,7 @@ Whisper = {
 	},
 
 	getLineType: function(line) {
-		return line ? line.getAttribute('ltype') : null;
+		return line ? line.getAttribute('data-line-type') : null;
 	},
 
 	coalesceLines: function(lineNum) {
@@ -60,88 +60,26 @@ Whisper = {
 			sender.innerHTML = '';
 		}
 	},
-	
+
 	enableEmoticonIfSet : function(lineNum){
 		var line = Whisper.getLine(lineNum);
 		var innerMessage = Whisper.getInnerMessage(line);
-		
+
 		if (innerMessage){
 			var text = innerMessage.innerHTML; 
 			if (text.indexOf("!whisper emoticons on") > -1){
 				app.styleSettingsSetValue("enableEmoticon", true);
 			}
-			
+
 			if (text.indexOf("!whisper emoticons off") > -1){
 				app.styleSettingsSetValue("enableEmoticon", false);
 			}
 		}
 	},
-	
-	
-	updateNicknameAssociatedWithNewMessage: function(e)
-	{
-		/* We only want to target plain text messages. */
-		var elementType = e.getAttribute("ltype");
-	
-		if (elementType == "privmsg" || elementType == "action") {
-			/* Get the nickname information. */
-			var senderSelector = e.querySelector(".sender");
-	
-			if (senderSelector) {
-				/* Is this a mapped user? */
-				var nickname = senderSelector.getAttribute("nickname");
-	
-				/* If mapped, toggle status on for new message. */
-				if (this.mappedSelectedUsers.indexOf(nickname) > -1) {
-					this.toggleSelectionStatusForNicknameInsideElement(senderSelector);
-				}
-			}
-		}
-	},
-	
-	toggleSelectionStatusForNicknameInsideElement: function(e)
-	{
-		/* e is nested as the .sender so we have to go three parents
-		 up in order to reach the parent div that owns it. */
-		var parentSelector = e.parentNode.parentNode.parentNode.parentNode;
-	
-		parentSelector.classList.toggle("selectedUser");
-	},
-	
-	userNicknameSingleClickEvent: function(e)
-	{
-		/* This is called when the .sender is clicked. */
-		var nickname = e.getAttribute("nickname");
-	
-		/* Toggle mapped status for nickname. */
-		var mappedIndex = this.mappedSelectedUsers.indexOf(nickname);
-	
-		if (mappedIndex == -1) {
-			this.mappedSelectedUsers.push(nickname);
-		} else {
-			this.mappedSelectedUsers.splice(mappedIndex, 1);
-		}
-	
-		/* Gather basic information. */
-		var documentBody = document.getElementById("body_home");
-	
-		var allLines = documentBody.querySelectorAll('div[ltype="privmsg"], div[ltype="action"]');
-	
-		/* Update all elements of the DOM matching conditions. */
-		for (var i = 0, len = allLines.length; i < len; i++) {
-			var sender = allLines[i].querySelectorAll(".sender");
-	
-			if (sender.length > 0) {
-				if (sender[0].getAttribute("nickname") === nickname) {
-					this.toggleSelectionStatusForNicknameInsideElement(sender[0]);
-				}
-			}
-		}
-	}
 };
 
 Emoticons = {
-	
+
 	list : {
 		";)"        : "Wink.png",
 		"X)~"        : "Facial.png",
@@ -172,53 +110,53 @@ Emoticons = {
 		"(DRUDGE)"  : "drudge.png",
 		"(CALTSAR)" : "caltsar.png"
 	},
-	
+
 	replaceEmoticonFromText : function (text) {
-		
+
 		if (text.match(/color:/ig)) {
 			return text;
 		}
-		
+
 		text = text.replace(/(^D-?:)|\s(D-?:)|(X-?\)~|&gt;:d|;-?\)|:-?\)|\(-?:|(:-?@)|:-?\[|:-?s|:&apos;-?\(|:-?\||:-?\(|:-?o|:-?D|o:-?\)|&lt;3|8-?\)|:-?\*|:-?&apos;\(|(:-?\$|:-?p|:-?\\|\(N\)|\(Y\)|\(NL\)|\(OKEANOS\)|\(DRUDGE\)|\(CALTSAR\)))/ig, function(emote){ return Emoticons.imageForEmoticon(emote) } );
-		
+
 		return text;
 	},
-	
-	
-	
+
+
+
 	imageForEmoticon : function (emote){
 		window.console.log('emote = "' + emote + '"');
 		var result = emote;
 		var imageName = Emoticons.list[emote.replace('-', '').toUpperCase()];
-	
+
 		if (imageName == null) return emote;
-	
+
 		switch(emote){
 			case ' D:':
 				result = '&nbsp;<img src="img/emoticons/' + imageName + '" class="emoticon" alt="'+emote+'" onclick="removeEmoticon(this, \'' + emote.replace('&apos;', 'WHISPERAPOS') + '\');" />';
 			default:
 				result = '<img src="img/emoticons/' + imageName + '" class="emoticon" alt="'+emote+'" onclick="removeEmoticon(this, \'' + emote.replace('&apos;', 'WHISPERAPOS') + '\');" />';
 		}
-	
+
 		return result;
 	},
-	
+
 	replaceTextWithEmoticons : function (lineNum) {
 		var line = Whisper.getLine(lineNum);
 		var innerMessage = Whisper.getInnerMessage(line);
-		
+
 		if (innerMessage){
 			var textWithEmoticon = Emoticons.replaceEmoticonFromText(innerMessage.innerHTML);
 			innerMessage.innerHTML = textWithEmoticon;	
 		}
-		
+
 	},
-	
+
 	isEnabled : function() {
 		return app.styleSettingsRetrieveValue("enableEmoticon");
 	}
-	
 }
+
 
 Textual.viewFinishedLoading = function()
 {
@@ -234,18 +172,13 @@ Textual.viewFinishedReload = function()
 	Textual.viewFinishedLoading();
 };
 
-Textual.newMessagePostedToView = function (lineNum) {
+Textual.messageAddedToView = function (lineNum, fromBuffer) {
 	Whisper.coalesceLines(lineNum);
-	//Whisper.enableEmoticonIfSet(lineNum);
-	// if(Emoticons.isEnabled()) {
-	// 	Emoticons.replaceTextWithEmoticons(lineNum);
-	// }
-	
-	var element = document.getElementById("line-" + lineNum);
-	Whisper.updateNicknameAssociatedWithNewMessage(element);
-};
+    var element = document.getElementById("line-" + lineNum);
+    ConversationTracking.updateNicknameWithNewMessage(element);
+}
 
 Textual.nicknameSingleClicked = function(e)
 {
-	Whisper.userNicknameSingleClickEvent(e);
+    ConversationTracking.nicknameSingleClickEventCallback(e);
 }
